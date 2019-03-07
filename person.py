137fb38646da2a80df2d2298631ddaa62ae1b08a
None <bogdanbbr2000@gmail.com>
2019-03-07 18:08:01 +0200

    lol

---
+++ person.py
@@ -0,0 +1,18 @@
+
+people = []
+class Person:
+    def __init__(self, Fname, Lname, age):
+        self.Fname = Fname
+        self.Lname = Lname
+        self.age = age
+        people.append(self)    
+Person("Bogdan", "Bogulov", 18)
+Person("Merkus", "XD", 17)
+Person("Bogdan", "Bogulov", 18)
+Person("Merkus", "XD", 17)
+Person("Bogdan", "Bogulov", 18)
+Person("Merkus", "XD", 17)
+Person("Bogdan", "Bogulov", 18)
+Person("Merkus", "XD", 17)
+Person("Bogdan", "Bogulov", 18)
+Person("Merkus", "XD", 17)
--- views.py
+++ views.py
@@ -1,8 +1,11 @@
 from app import app
 from flask import render_template, request, redirect, url_for
-
+from person import Person, people
 
 @app.route('/') 
 def home():
-    return render_template('Home.html')
+    return render_template('Home.html', title="HomePage__", people=people )
 
+@app.route('/info') 
+def info():
+    return render_template('Info.html')
